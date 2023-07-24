58a73578854087f0243b9429c45a3a274c0eaccf
fahimrimon <fahimrimon21@gmail.com>
2023-07-24 18:17:14 +0600

    filtered by year

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { IProduct } from "../types/GlobalTypes";
 import BookCard from "../components/BookCard";
 import { useGetBooksQuery } from "../redux/features/book/bookApi";
+import { ToastContainer, toast } from 'react-toastify';
 
 function AllBooks() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("");
+
   const {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     data: allBooks,
@@ -22,6 +28,53 @@ function AllBooks() {
   //     .then((data) => setData(data));
   // }, []);
 
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchQueryValue = e.target.value.toLowerCase();
+    setSearchQuery(searchQueryValue);
+  };
+
+  const handleYear = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedYearValue = e.target.value;
+    setSelectedYear(selectedYearValue);
+  };
+
+  const handleGenre = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedGenreValue = e.target.value;
+    setSelectedGenre(selectedGenreValue);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const filteredBooks = allBooks?.data.filter((book: IProduct) => {
+    const isMatchingSearch =
+      book.name.toLowerCase().includes(searchQuery) ||
+      book.author.toLowerCase().includes(searchQuery) ||
+      book.genre.toLowerCase().includes(searchQuery);
+    const isMatchingYear =
+      selectedYear === "" || (book.publicationYear).toString() === selectedYear;
+    const isMatchingGenre =
+      selectedGenre === "" || book.genre === selectedGenre;
+    return isMatchingSearch && isMatchingYear && isMatchingGenre;
+  });
+
+  const genreOptions: string[] = Array.from(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    new Set(allBooks?.data.map((book: IProduct) => book.genre))
+  );
+
+  const yearOptions: number[] = Array.from(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    new Set(allBooks?.data.map((book: IProduct) => book.publicationYear))
+  );
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  if (isError) {
+    toast.error("Something went wrong");
+  }
+
+
   return (
     <div className="mx-20 mt-8">
       <h2 className="text-3xl text-center font-bold mb-6">All Books</h2>
@@ -29,7 +82,9 @@ function AllBooks() {
         <p className="sm:text-center lg:mb-0 mb-4">
           <input
             type="text"
-            placeholder="Type here"
+            placeholder="Search your favourite book"
+            value={searchQuery}
+            onChange={handleSearch}
             className="input input-bordered w-72"
           />
         </p>
@@ -53,28 +108,25 @@ function AllBooks() {
         <label className="label">
     <span className="label-text">By Publication Year</span>
   </label>
-          <select className="select select-bordered w-32">
-            <option disabled selected>
+          <select value={selectedYear}
+                  onChange={handleYear} className="select select-bordered w-32">
+            <option value="">
             Dafault
             </option>
-            <option>2014</option>
-            <option>2015</option>
-            <option>2016</option>
-            <option>2017</option>
-            <option>2018</option>
-            <option>2019</option>
-            <option>2020</option>
-            <option>2021</option>
-            <option>2022</option>
-            <option>2023</option>
+            {yearOptions.map((year: number) => (
+                    <option value={year} key={year}>
+                      {year}
+                    </option>
+                  ))}
           </select>
         </p>
       </div>
       <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 my-20 gap-x-8 gap-y-8 ">
         {/* // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */}
-        {allBooks?.data?.map((product:IProduct, i:number) => (
+        {filteredBooks?.map((product:IProduct, i:number) => (
           <BookCard product={product} key={i}></BookCard>
         ))}
+        <ToastContainer />
       </div>
     </div>
   );
