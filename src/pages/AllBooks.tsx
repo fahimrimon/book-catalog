2e10e32e1478930bfb7e2e303c5ae0f59c5aa2d4
fahimrimon <fahimrimon21@gmail.com>
2023-07-24 18:26:22 +0600

    filtered by genre

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -4,10 +4,9 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { IProduct } from "../types/GlobalTypes";
 import BookCard from "../components/BookCard";
 import { useGetBooksQuery } from "../redux/features/book/bookApi";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
 function AllBooks() {
-
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedYear, setSelectedYear] = useState("");
   const [selectedGenre, setSelectedGenre] = useState("");
@@ -50,7 +49,7 @@ function AllBooks() {
       book.author.toLowerCase().includes(searchQuery) ||
       book.genre.toLowerCase().includes(searchQuery);
     const isMatchingYear =
-      selectedYear === "" || (book.publicationYear).toString() === selectedYear;
+      selectedYear === "" || book.publicationYear.toString() === selectedYear;
     const isMatchingGenre =
       selectedGenre === "" || book.genre === selectedGenre;
     return isMatchingSearch && isMatchingYear && isMatchingGenre;
@@ -74,61 +73,66 @@ function AllBooks() {
     toast.error("Something went wrong");
   }
 
-
   return (
-    <div className="mx-20 mt-8">
-      <h2 className="text-3xl text-center font-bold mb-6">All Books</h2>
-      <div className="lg:flex items-center justify-center space-x-6 -mb-12">
-        <p className="sm:text-center lg:mb-0 mb-4">
-          <input
-            type="text"
-            placeholder="Search your favourite book"
-            value={searchQuery}
-            onChange={handleSearch}
-            className="input input-bordered w-72"
-          />
-        </p>
-        <p className="mb-4 lg:mb-9">
-        <label className="label">
-    <span className="label-text">By Genre</span>
-  </label>
-  <select className="select select-bordered w-32">
-            <option disabled selected>
-              Dafault
-            </option>
-            <option>History</option>
-            <option>Fantasy</option>
-            <option>Cooking</option>
-            <option>Science</option>
-            <option>Psychology</option>
-            <option>Mystery</option>
-          </select>
-        </p>
-        <p className="mb-0 lg:mb-9">
-        <label className="label">
-    <span className="label-text">By Publication Year</span>
-  </label>
-          <select value={selectedYear}
-                  onChange={handleYear} className="select select-bordered w-32">
-            <option value="">
-            Dafault
-            </option>
-            {yearOptions.map((year: number) => (
-                    <option value={year} key={year}>
-                      {year}
-                    </option>
-                  ))}
-          </select>
-        </p>
-      </div>
-      <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 my-20 gap-x-8 gap-y-8 ">
-        {/* // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */}
-        {filteredBooks?.map((product:IProduct, i:number) => (
-          <BookCard product={product} key={i}></BookCard>
-        ))}
-        <ToastContainer />
-      </div>
-    </div>
+    <>
+      {isSuccess && (
+        <div className="mx-20 mt-8">
+          <h2 className="text-3xl text-center font-bold mb-6">All Books</h2>
+          <div className="lg:flex items-center justify-center space-x-6 -mb-12">
+            <p className="sm:text-center lg:mb-0 mb-4">
+              <input
+                type="text"
+                placeholder="Search your favourite book"
+                value={searchQuery}
+                onChange={handleSearch}
+                className="input input-bordered w-72"
+              />
+            </p>
+            <p className="mb-4 lg:mb-9">
+              <label className="label">
+                <span className="label-text">By Genre</span>
+              </label>
+              <select
+                value={selectedGenre}
+                onChange={handleGenre}
+                className="select select-bordered w-32"
+              >
+                <option value="">Dafault</option>
+                {genreOptions.map((genre: string) => (
+                  <option value={genre} key={genre}>
+                    {genre}
+                  </option>
+                ))}
+              </select>
+            </p>
+            <p className="mb-0 lg:mb-9">
+              <label className="label">
+                <span className="label-text">By Publication Year</span>
+              </label>
+              <select
+                value={selectedYear}
+                onChange={handleYear}
+                className="select select-bordered w-32"
+              >
+                <option value="">Dafault</option>
+                {yearOptions.map((year: number) => (
+                  <option value={year} key={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+            </p>
+          </div>
+          <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 my-20 gap-x-8 gap-y-8 ">
+            {/* // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */}
+            {filteredBooks?.map((product: IProduct, i: number) => (
+              <BookCard product={product} key={i}></BookCard>
+            ))}
+            <ToastContainer />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
